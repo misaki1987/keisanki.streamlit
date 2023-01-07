df44c6260df3fb6e1a6c8b256b28f5271ee4ad14
misaki1987 <46012527+misaki1987@users.noreply.github.com>
2023-01-07 13:04:30 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,26 +6,36 @@ import time
 
 st.title("streamlitで作った計算機")
 """
-## 自賠責基準
-## 赤本基準
+## 基礎情報
 """
-
-option = st.selectbox(
-    "あなたの好きな数字をおしえてください。",
-    list(range(1,11))
+hosp = st.sidebar.selectbox(
+    "あなたの入院日数をおしえてください。",
+    list(range(1,31))
 )
+gtth = st.sidebar.selectbox(
+    ""
+    list(range(1,31))
+)
+condition = st.sidebar.slider("過失割合は？",0,100,50)
 
-"あなたが好きな数字は、" ,option, "です。"
-
-condition = st.slider("過失割合は？",0,100,50)
+"入院日数：" ,hosp
+"通院日数：" ,gtth
 "過失割合：" ,condition
 
-#エクスパンダーは拡張的なものFAQなど
-expander1 = st.expander("計算式１")
-expander1.write("計算式の内容")
-expander2 = st.expander("計算式2")
-expander2.write("計算式の内容")
-expander3 = st.expander("計算式3")
-expander3.write("計算式３")
+"""
+## 自賠責基準（別表Ⅰ）
+"""
+"入院慰謝料：", 530000 * hosp / 30 
+"通院慰謝料：", gtth
+ex1 = st.expander("計算式１")
+ex1.write("自賠責基準の場合、慰謝料は１日４３００円です。")
+
+"""
+## 赤本基準
+"""
+"入院慰謝料：", hosp * 4300
+"通院慰謝料：", gtth * 4300
+ex1 = st.expander("計算式１")
+ex1.write("自賠責基準の場合、慰謝料は１日４３００円です。")
 
 st.write("作成中")
